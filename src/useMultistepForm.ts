229401715c7af5af9a5e38492db02df10854ee95
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-10-25 19:31:30 +0530

    Add useMultistepForm hook for managing multistep forms

---
+++ src/useMultistepForm.ts
@@ -0,0 +1,35 @@
+import { ReactElement, useState } from "react";
+
+export function useMultistepForm(steps: ReactElement[]) {
+
+    const[currentFromIndex, setCurrentFromIndex] = useState(0)
+  
+    function next(){
+        setCurrentFromIndex(i => {
+            if(i >= steps.length - 1){
+                return i
+            }
+            return i + 1
+        })
+    }
+
+    function back(){
+        setCurrentFromIndex(i => {
+            if(i <= 0){
+                return i
+            }
+            return (i - 1)
+        })
+    }
+
+    function goto(idx: number){
+        setCurrentFromIndex(idx)
+    }
+
+  return {
+    step: steps[currentFromIndex],
+    next,
+    back,
+    goto
+  };
+}
