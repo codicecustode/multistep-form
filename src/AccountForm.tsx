e4bcfcd5405d84f9164d1ebd9929d823a4b2e3c5
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-10-26 10:29:13 +0530

    Add AccountForm component for capturing account information

---
+++ src/AccountForm.tsx
@@ -0,0 +1,31 @@
+export function AccountForm(){
+    return(
+        <>
+            <label>Email</label>
+            <input 
+                autoFocus 
+                required 
+                type="text"
+            />
+
+            <label>Contact No.</label>
+            <input 
+                required 
+                type="number"
+            />
+
+            <label>Username</label>
+            <input 
+                required  
+                type="number"
+            />
+
+            <label>Password</label>
+            <input 
+                required 
+                type="text"
+            />
+        </>
+    )
+    
+}
